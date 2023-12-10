2e3ce255b192a1e1b2e71784995d62380df93824
Asia <107987407+aauville@users.noreply.github.com>
2023-12-10 17:52:34 +0900

    added stations that spawn and destroy characters, and score feature

---
+++ shibuya station game/Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MyCharacterController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    [SerializeField]
+    public float lineSize = 1f;
+    private bool isDragging = false;
+    private List<Vector2> pathPoints = new List<Vector2>();
+    private GameObject destinationStation; 
+
+    private SpriteRenderer spriteRenderer;
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.positionCount = 0;
+        lineRenderer.startWidth = lineSize;
+        lineRenderer.endWidth = lineSize;
+        lineRenderer.numCapVertices = 30;
+        lineRenderer.numCornerVertices = 30;
+        lineRenderer.startColor = Color.red;
+        lineRenderer.endColor = Color.red;
+        lineRenderer.textureMode = LineTextureMode.Tile;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D hitCollider = Physics2D.OverlapPoint(touchPosition);
+
+            if (hitCollider != null && hitCollider.gameObject == gameObject)
+            {
+                isDragging = true;
+                ClearPath();
+            }
+        }
+
+        if (isDragging && Input.GetMouseButton(0))
+        {
+            Vector2 currentTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pathPoints.Add(currentTouchPosition);
+            lineRenderer.positionCount = pathPoints.Count;
+            lineRenderer.SetPositions(ConvertVector2ToVector3(pathPoints.ToArray()));
+            UpdateLineTransparency();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        if (pathPoints.Count > 0)
+        {
+            Vector2 targetPosition = pathPoints[0];
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
+            {
+                pathPoints.RemoveAt(0);
+            }
+
+            lineRenderer.positionCount = pathPoints.Count;
+            lineRenderer.SetPositions(ConvertVector2ToVector3(pathPoints.ToArray()));
+            UpdateLineTransparency();
+
+            // Vérifie si le personnage est arrivé à destination
+            ArrivedAtDestination();
+        }
+    }
+
+    private void ClearPath()
+    {
+        pathPoints.Clear();
+        lineRenderer.positionCount = 0;
+    }
+
+    private Vector3[] ConvertVector2ToVector3(Vector2[] vector2Array)
+    {
+        Vector3[] vector3Array = new Vector3[vector2Array.Length];
+        for (int i = 0; i < vector2Array.Length; i++)
+        {
+            vector3Array[i] = new Vector3(vector2Array[i].x, vector2Array[i].y, 0f);
+        }
+        return vector3Array;
+    }
+
+    private void UpdateLineTransparency()
+    {
+        for (int i = 0; i < pathPoints.Count; i++)
+        {
+            float distance = Vector2.Distance(transform.position, pathPoints[i]);
+            float transparency = Mathf.Clamp01(1f - distance / 5f);
+            Color color = lineRenderer.startColor;
+            color.a = transparency;
+            lineRenderer.startColor = color;
+            lineRenderer.endColor = color;
+        }
+    }
+
+    public void SetDestination(GameObject destination)
+        {
+            destinationStation = destination;
+        }
+
+
+    private void ArrivedAtDestination()
+    {
+        if (IsAtCorrectStation(destinationStation.GetComponent<CircleCollider2D>()))
+        {
+            GameManager.Instance.IncrementScore();
+            Destroy(gameObject);
+        }
+    }
+
+    private bool IsAtCorrectStation(CircleCollider2D stationCollider)
+    {
+        return stationCollider.OverlapPoint(transform.position);
+    }
+}
---
+++ shibuya station game/Assets/Scripts/StationController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class StationController : MonoBehaviour
+{
+    public GameObject CharacterPrefab; 
+    public float minSpawnInterval = 10f; // Intervalle minimal entre les apparitions de personnages
+    public float maxSpawnInterval = 20f; // Intervalle maximal entre les apparitions de personnages
+    private GameObject[] allStations; 
+    private bool isCharacterSpawning = false;
+
+    void Start()
+    {
+        allStations = GameObject.FindGameObjectsWithTag("Station");
+        isCharacterSpawning = true;
+        StartCoroutine(SpawnCharacter());
+    }
+
+    IEnumerator SpawnCharacter()
+    {
+        while (isCharacterSpawning)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnInterval, maxSpawnInterval));
+            GameObject destinationStation = GetRandomDestinationStation();
+            GameObject character = Instantiate(CharacterPrefab, transform.position, Quaternion.identity);
+            character.GetComponent<MyCharacterController>().SetDestination(destinationStation);
+        }
+    }
+
+    GameObject GetRandomDestinationStation()
+    {
+        GameObject destination = allStations[Random.Range(0, allStations.Length)];
+        while (destination == gameObject)
+        {
+            destination = allStations[Random.Range(0, allStations.Length)];
+        }
+        return destination;
+    }
+
+
+
+}
